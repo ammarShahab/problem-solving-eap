60568deed6ec9c32ffb88d80cab68d40eab2e9cd
ammarShahab <ashahab007@gmail.com>
2026-05-13 01:07:41 +0600

    solved 7 number problem

--- problem1_5.js
+++ problem1_5.js
@@ -13,3 +13,27 @@ function reverseSentence(sentence) {
 }
 
 reverseSentence("Hello Ammar!");
+
+/*Problem-7:
+Count the number of vowels in a string
+Write a function to count all vowels (a, e, i, o, u) in a given string. */
+
+function CountVowels(sentence) {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+  //   const sentence = "My name is Ammar";
+
+  const arr = sentence.split("");
+
+  let count = 0;
+
+  for (const element of arr) {
+    if (vowels.includes(element)) {
+      count++;
+    }
+  }
+
+  console.log(count);
+}
+
+CountVowels("My name is Ammar");
